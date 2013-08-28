a4f202728020d2710ff446d06c0f1bcbbbefad07
Jeremy Martin <jmar777@gmail.com>
2013-08-28 15:35:43 -0400

    adding lint task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,11 +16,28 @@ module.exports = function(grunt) {
 				src: 'jquery.kwicks.css',
 				dest: 'jquery.kwicks.min.css'
 			}
+		},
+		jshint: {
+			file: ['jquery.kwicks.js'],
+			options: {
+				camelcase: true,
+				eqeqeq: true,
+				es3: true,
+				undef: true,
+				unused: true,
+				trailing: true,
+				boss: true,
+				browser: true,
+				globals: {
+					jQuery: true
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	
 	grunt.registerTask('default', ['uglify', 'cssmin']);
 };
